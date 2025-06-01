dd4c63f57421077ebb02c31da1c22e8644de5ae0
phlabsdevops <harinathbeeram1999@gmail.com>
2025-06-01 19:09:04 +0530

    create ec2 using length function

--- terraform/count-sample/ec2.tf
+++ terraform/count-sample/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web"{
-    count=11
+    #count=11
+    count=length(var.instance-name)
     ami = var.ami-id #ami
     instance_type = var.instance-type
     #vpc_security_group_ids = [ aws_security_group.roboshop-all.id ]
