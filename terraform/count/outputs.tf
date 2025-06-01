65f48ecbae9ce3d0b289913b3f7035e651f59adb
phlabsdevops <harinathbeeram1999@gmail.com>
2025-06-01 17:43:07 +0530

    count with output file. we got all ec2 private ips and istance ids. in output file we can't use count.index

--- terraform/count/outputs.tf
+++ terraform/count/outputs.tf
@@ -1,7 +1,7 @@
 output "instance_private_ip_0" {
    value= aws_instance.web[0].private_ip
  }
- output "instance_private_i_1" {
+ output "instance_private_ip_1" {
    value= aws_instance.web[1].private_ip
  }
  output "instance_private_ip_2" {
@@ -12,6 +12,21 @@ output "instance_private_ip_0" {
  }
  output "instance_private_ip_4" {
    value= aws_instance.web[4].private_ip
+ }
+ output "instance_id_0" {
+   value= aws_instance.web[0].id
+ }
+ output "instance_id_1" {
+   value= aws_instance.web[1].id
+ }
+ output "instance_id_2" {
+   value= aws_instance.web[2].id
+ }
+ output "instance_id_3" {
+   value= aws_instance.web[3].id
+ }
+ output "instance_id_4" {
+   value= aws_instance.web[4].id
  }
 #  output "instance_private_ip" {
 #    value= aws_instance.web
