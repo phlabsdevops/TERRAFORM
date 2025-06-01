1090e429eea4d3385a33defa51d4e98deef10f0a
phlabsdevops <harinathbeeram1999@gmail.com>
2025-06-01 18:36:25 +0530

    ec2 and route 53 combo. webserver has public ip. webserver and rabbitmq has t2.micro

--- terraform/count/variables.tf
+++ terraform/count/variables.tf
@@ -6,9 +6,13 @@ variable "ami-id" {
   type=string
   default="ami-09c813fb71547fc4f"
 }
+# variable "instance-type" {
+#   type=string
+#   default="t3.micro"
+# }
 variable "instance-type" {
-  type=string
-  default="t3.micro"
+  type=list
+  default=["t2.micro" , "t3.medium"]
 }
 variable "tags" {
   type=map
@@ -18,4 +22,12 @@ variable "tags" {
     Environment="DEV"
     Terraform=true
   }
+}
+variable "zone-id" {
+  type=string
+  default="Z049815825KWUI6TFN3N8"
+}
+variable "domain-name" {
+  type=string
+  default="phlabsdevops.site"
 }
