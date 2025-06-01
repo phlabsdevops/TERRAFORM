5877181e93b99f12961b0717728a5035f6d4b946
phlabsdevops <harinathbeeram1999@gmail.com>
2025-06-01 17:40:08 +0530

    count with output file. we got all ec2 private ips

---
+++ terraform/count/outputs.tf
@@ -0,0 +1,18 @@
+output "instance_private_ip_0" {
+   value= aws_instance.web[0].private_ip
+ }
+ output "instance_private_i_1" {
+   value= aws_instance.web[1].private_ip
+ }
+ output "instance_private_ip_2" {
+   value= aws_instance.web[2].private_ip
+ }
+ output "instance_private_ip_3" {
+   value= aws_instance.web[3].private_ip
+ }
+ output "instance_private_ip_4" {
+   value= aws_instance.web[4].private_ip
+ }
+#  output "instance_private_ip" {
+#    value= aws_instance.web
+#  }
